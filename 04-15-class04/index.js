cbc2e500a2956d1ad881b53540d0b58095776816
Alli Campbelll <allison.campbell@ou.edu>
2017-04-15 11:32:23 -0500

    setting up a client with a restful api

--- 04-15-class04/index.js
+++ 04-15-class04/index.js
@@ -1,7 +1,13 @@
+/*set up server with '/' path, provides a client, while any of the other paths (email path)
+provide a restful api */
+
+
+
 
 var restify = require('restify');
 var server =  restify.createServer();
 var port = 8088;
+var fs = require('fs');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/enron');
@@ -24,6 +30,8 @@ var schema = new Schema({
     date: Date,
     subject: String
 });
+// what collection is schema assocted with
+var Emails = mongoose.model('emails', schema);
 
 db.on('error', function(msg){
     console.log('Mongoose connection error %s', msg);
@@ -32,6 +40,40 @@ db.on('error', function(msg){
 db.once('open', function(){
     console.log('Mongoose connection established');
 });
+
+//write function to GET emails
+function getEmails(req, res, next){
+    Emails.find().limit(10).exec(function(err,data){
+        if(err){res.send('Error;');}
+        else {
+            res.json(data);
+        }
+    });
+    return next();
+}
+
+// client
+server.get('/', function(req, res, next){
+    fs.readFile('index.html', function(err, data){
+        if(err){
+            console.log("Cannot read file index.html");
+            res.send(404);
+        } else {
+            var body = data.toString();
+            res.writeHead(200, {
+                'Content-Length' : Buffer.byteLength(body),
+                'Content-Type' : 'text/html'
+            });
+            res.write(body);
+            res.end();
+        }
+        return next ();
+    });
+});
+
+//server responses
+server.get('/emails',getEmails);
+
 server.get('/', function(req, res, next){
     res.send("Hello World");
     next();
