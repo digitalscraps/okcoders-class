6372ee29f9603971db122b9f7528979bddd2589a
Alli Campbelll <allison.campbell@ou.edu>
2017-04-15 10:51:04 -0500

    update mongoose schema

--- 04-15-class04/index.js
+++ 04-15-class04/index.js
@@ -3,7 +3,35 @@ var restify = require('restify');
 var server =  restify.createServer();
 var port = 8088;
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/enron');
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
 
+var schema = new Schema({
+    _id: Schema.Types.ObjectId,
+    sender: String,
+    recipients: [],
+    cc: [],
+    text: String,
+    mid: String,
+    fpath: String,
+    bcc: [],
+    to: [],
+    replyto: Schema.Types.Mixed,
+    ctype: String,
+    fname: String,
+    date: Date,
+    subject: String
+});
+
+db.on('error', function(msg){
+    console.log('Mongoose connection error %s', msg);
+});
+
+db.once('open', function(){
+    console.log('Mongoose connection established');
+});
 server.get('/', function(req, res, next){
     res.send("Hello World");
     next();
