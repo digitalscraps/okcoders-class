f6997a264713c206b448c87e200d499535e9ae5c
Alli Campbelll <allison.campbell@ou.edu>
2017-04-29 10:57:39 -0500

    adding authentication routes

--- 05-29-class06/routes/auth/index.js
+++ 05-29-class06/routes/auth/index.js
@@ -1,5 +1,11 @@
 var Auth = require('../../models/auth');
 
 exports.create = function(req, res, next) {
-    
+    var auth = new Auth ();
+    auth.username = req.params.username;
+    auth.password = req.params.password;
+    auth.date = new Date();
+    auth.is_active =true;
+
+    auth.save
 }
